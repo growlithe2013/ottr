e86aa200cba00953f6a5661950ea877157472bdb
chris baker <crystal_baker@mail.tmcc.edu>
2025-09-17 20:56:48 -0700

    test for seamless chat experience.

--- main.py
+++ main.py
@@ -23,8 +23,24 @@ def main():
 
     match (len(sys.argv)):
         case 1:
-
-            sys.exit(1)
+            response=[]
+            while True:
+                if len(response)>19:
+                    response.pop(0)
+                userIn = input("What is your question? Q to quit, S for settings, enter prompt:")
+                if userIn.upper()=="Q":
+                    sys.exit(0)
+                elif userIn.upper()=="S":
+                    print("There is currently nothing configurable.")
+                else:
+                    while done==False and i<20:
+                        response.append(chat(None, True))
+                        if response[-1].text != None:
+                            done=True
+                        i+=1
+                    print(response[-1].text)
+                    
+            
         
         case 2:
             response=[]
